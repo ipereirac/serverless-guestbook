d715cc2683ba9b41dddb24268150f0c1d2068071
ipereirac <ipereirac@users.noreply.github.com>
2022-07-28 09:00:21 -0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://7c5d4baf.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
